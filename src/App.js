0e99e46edc34bfba879ef808c0fc1b15061c00ac
jcarrot-1008 <jcarrot1008@naver.com>
2022-07-14 00:11:52 +0900

    Calss, Style
    Calss, Style, 주석

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Hello from './Component/hello';
+import './App.css';
 
 function App() {
+  const name = 'Hi. hello world';
+  const style = {
+    backgroundColor: 'black',
+    color: 'red',
+    fontSize: '10px',
+    padding: '1rem',
+  }
   return (
-    <div className="App">
+    <div style={style}>
+      {/* 주석주석주석 */}
       <Hello />
+      <div>{name}</div>
+      <div className='box'></div>
     </div>
   );
 }
