df664069c3bb3758131733c7952b2e2de224df90
jcarrot-1008 <jcarrot1008@naver.com>
2022-07-16 01:35:40 +0900

    back..

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import Hello from './Component/hello';
-import './App.css';
+import Hello from "./Component/hello";
+import "./App.css";
 
 function App() {
-  const name = 'Hi. hello world';
-  const style = {
-    backgroundColor: 'black',
-    color: 'red',
-    fontSize: '10px',
-    padding: '1rem',
-  }
+
+
+
+
   return (
-    <div style={style}>
-      {/* 주석주석주석 */}
-      <Hello />
-      <div>{name}</div>
-      <div className='box'></div>
+    <div className="App">
+      <div className="Nav">안녕하세요</div>
     </div>
   );
 }
